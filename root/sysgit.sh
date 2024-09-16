c3033c8f2b5d0e3bc6cc232d19865bc8731db1e1
starvingarden <github@ksan314.simplelogin.com>
2024-09-15 20:51:28 -0500

    update scripts

---
+++ root/sysgit.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# this script manages system files with git
+
+# run by appending either "push" to push system file changes to github, or "pull" to pull system file changes from github
+
+action=$1
+
+
+
+
+
+# check that script is running as root
+######################################
+
+currentUser=$(whoami)
+if [ "$currentUser" == root ]
+then
+    continue
+else
+    echo "this script must be run as root"
+    exit
+fi
+
+
+
+
+
+# configure the git repo
+########################
+
+# create the /.sysgit directory if it doesn't exist
+mkdir /.sysgit
+
+# clear the /.sysgit directory
+rm -rf /.sysgit/*
+
+# initialize the git repo
+git init /.sysgit
+
+# set the remote repo
+git --git-dir=/.sysgit/.git --work-tree=/.sysgit remote add origin https://github.com/starvingarden/system
+
+
+
+
+
+# push system file changes to github
+####################################
+
+if [ "$action" == push ]
+then
+
+    # set permissions for scripts
+    #############################
+
+    #chown -R root:root /scripts
+    #chmod -R 755 /scripts/all
+    #chmod 755 /scripts/root
+    #chmod 754 /scripts/root/*
+
+
+    
+    # copy system files to the /.sysgit directory
+    #############################################
+
+    #cp -r /scripts/* /.scriptgit
+
+
+
+    # push system files to online git repo
+    ######################################
+
+    git --git-dir=/.sysgit/.git --work-tree=/.sysgit add .
+    git --git-dir=/.sysgit/.git --work-tree=/.sysgit commit -m "update system files"
+    git --git-dir=/.sysgit/.git --work-tree=/.sysgit push -f origin main
+
+fi
+
+
+
+
+
+# pull system file changes from github
+######################################
+
+if [ "$action" == pull ]
+then
+
+    # pull system files from online git repo
+    ########################################
+
+    git --git-dir=/.sysgit/.git --work-tree=/.sysgit pull origin main
+
+
+
+    # copy system files from /.sysgit directory to appropriate locations
+    ####################################################################
+
+    #cp -r /.sysgit/* /scripts
+
+
+
+    # set permissions for system files
+    ##################################
+
+    #chown -R root:root /scripts
+    #chmod -R 755 /scripts/all
+    #chmod 755 /scripts/root
+    #chmod 754 /scripts/root/*
+
+fi
